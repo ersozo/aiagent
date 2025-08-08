728d32ed1e33923cdc10a7ef7d794fa87e1ecf88
Ozgur Ersoz <ozgur.ersoz3@gmail.com>
2025-08-08 22:57:40 +0300

    verbose

--- main.py
+++ main.py
@@ -15,7 +15,14 @@ def main():
         print('\nUsage: python main.py "your prompt here"')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
+
+    verbose = False
+    if "--verbose" in args:
+        verbose = True
+        args.remove("--verbose")
     user_prompt = " ".join(args)
+    if verbose:
+        print(f"User prompt: {user_prompt}")
 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
@@ -26,18 +33,23 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    generate_content(client, messages)
+    generate_content(client, messages, verbose)
 
 
-def generate_content(client, messages):
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
     )
+    if verbose:
+        prompt_tokens = response.usage_metadata.prompt_token_count
+        response_tokens = response.usage_metadata.candidates_token_count
+
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
 
     print("Response:")
     print(response.text)
-
 
 if __name__ == "__main__":
     main()
